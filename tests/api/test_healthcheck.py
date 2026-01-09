c041e7f9a76dd4fb2369c777fef97b97a678734d
Victor André Machado de Lara <victorlara2010@gmail.com>
2026-01-09 20:07:06 -0300

    Add parametrized API healthcheck tests

--- tests/api/test_healthcheck.py
+++ tests/api/test_healthcheck.py
@@ -1,5 +1,12 @@
+import pytest
 import requests
 
-def test_api_healthcheck():
-    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+@pytest.mark.parametrize("endpoint", [
+    "/posts",
+    "/comments",
+    "/users"
+])
+def test_api_endpoints_healthcheck(base_url, endpoint):
+    response = requests.get(f"{base_url}{endpoint}")
     assert response.status_code == 200
+    assert response.json() is not None
