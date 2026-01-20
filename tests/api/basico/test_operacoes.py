c470ce3d147d204829dfca6278258b334e5e41ec
Victor André Machado de Lara <victorlara2010@gmail.com>
2026-01-19 21:02:51 -0300

    marcadores

---
+++ tests/api/basico/test_operacoes.py
@@ -0,0 +1,29 @@
+import pytest
+import time
+
+
+def soma(a , b):
+    return a + b
+
+def multiplica(a,b):
+    return a * b
+
+
+@pytest.mark.lento
+def test_soma_lenta():
+    time.sleep(2)
+    assert soma(2,2) == 4
+
+
+@pytest.mark.rapido
+def test_soma_rapida():
+    assert soma(2,3) == 5
+
+@pytest.mark.lento
+def test_multiplicao_lenta():
+    time.sleep(2)
+    assert multiplica(3,3) == 9
+
+@pytest.mark.rapido
+def test_multiplicao_rapido():
+    assert multiplica(3,3) == 9
