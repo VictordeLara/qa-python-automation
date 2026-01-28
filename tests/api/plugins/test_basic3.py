3bd5cea727222b19c9dcf5560c88795e987a7122
Victor André Machado de Lara <victorlara2010@gmail.com>
2026-01-28 19:13:40 -0300

    cov

---
+++ tests/api/plugins/test_basic3.py
@@ -0,0 +1,11 @@
+from funcoes import *
+
+
+def test_email_valido():
+    assert email_valido("exemplo@gmail.com") is True
+    assert email_valido("exemplo.com") is False
+'''
+def test_dividir():
+    assert divdir(4,2) == 2
+    assert divdir(4,0) is None
+'''
---
+++ tests/api/plugins/test_estruturais.py
@@ -0,0 +1,6 @@
+def test_listas_iguais():
+
+    lista_esperada = [ 1, 2, 3, 4, 5]
+    lista_resultado = [ 1, 2, 3, 4, 5]
+    
+    assert lista_esperada == lista_resultado, "As listas não são iguais"
